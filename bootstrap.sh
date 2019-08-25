118fc6f533a38d74534f6e8096407a3b85eb6f89
imaginary-upside <imaginary-upside@protonmail.ch>
2019-08-25 19:44:36 -0400

    Create client_login without trailing newline

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ ! -f /opt/hentai/data/client_login ]; then
   mkdir -p /opt/hentai/data
-  echo "$CLIENT_ID"-"$CLIENT_KEY" >> /opt/hentai/data/client_login
+  echo -n "$CLIENT_ID"-"$CLIENT_KEY" >> /opt/hentai/data/client_login
 fi
 
 java -jar /opt/hentai/HentaiAtHome.jar
